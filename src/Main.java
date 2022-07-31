13b77e04827d50c912d2c33353f3fc9143fcd6f0
h110401 <hung110401@gmail.com>
2022-07-31 22:26:43 +0700

    bt2

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student student = new Student();
+
+        student.setName("Jane Doe");
+
+        student.setClasses("RA");
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,16 @@
+public class Student {
+    private String name = "John";
+    private String classes = "CO2";
+
+    public Student() {
+
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
